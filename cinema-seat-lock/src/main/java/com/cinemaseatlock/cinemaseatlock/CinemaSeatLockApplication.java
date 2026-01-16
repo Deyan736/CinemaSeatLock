b839a9b362ed769549a47923130f4789b4612e49
Deyan736 <dimdeki@gmail.com>
2026-01-16 17:53:14 +0200

    chore: init CinemaSeatLock Spring Boot project

---
+++ cinema-seat-lock/src/main/java/com/cinemaseatlock/cinemaseatlock/CinemaSeatLockApplication.java
@@ -0,0 +1,13 @@
+package com.cinemaseatlock.cinemaseatlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSeatLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaSeatLockApplication.class, args);
+	}
+
+}
---
+++ cinema-seat-lock/src/test/java/com/cinemaseatlock/cinemaseatlock/CinemaSeatLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemaseatlock.cinemaseatlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaSeatLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
